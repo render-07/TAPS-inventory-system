2fc119aa89cb043be761a6fe3a320f80162d4cd8
Redner Ivan Cabra <rednerivanpadasaycabra.stack@gmail.com>
2022-03-05 20:40:07 +0800

    Changes for heroku deployment

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -43,7 +43,7 @@ export default function Home() {
           />
           {showAddCategory && <AddCategory />}
           {/*<Categories categories = {categories} onDelete = {deleteCategory} onToggle = {toggleReminder}/>*/}
-          <Categories />
+          {/* <Categories /> */}
         </div>
         <div className="rec-upper-h"></div>
         <div className="rec-lower-left-h"></div>
