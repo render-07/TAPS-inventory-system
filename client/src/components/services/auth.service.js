d9c720e1054d95e2af8287d60fcf518fa68142b6
Redner Ivan Cabra <rednerivanpadasaycabra.stack@gmail.com>
2022-03-05 20:30:24 +0800

    Changes for heroku deployment

--- client/src/components/services/auth.service.js
+++ client/src/components/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "/api";
+const baseURL = "https://technocashierautomatedproduct.herokuapp.com/api";
 
 // register request
 const register = (newUser) => {
