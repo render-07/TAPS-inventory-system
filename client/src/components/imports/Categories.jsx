aea6701fc21bd18915c1865ed20afcc24b4e4b70
Redner Ivan Cabra <rednerivanpadasaycabra.stack@gmail.com>
2022-03-05 20:52:13 +0800

    Changes for heroku deployment

--- client/src/components/imports/Categories.jsx
+++ client/src/components/imports/Categories.jsx
@@ -1,39 +1,41 @@
-import { useEffect } from 'react'
-import SingleCategory from './SingleCategory'
-import {  useDispatch, useSelector } from 'react-redux';
-import { getCategories } from '../../container/categoryActions';
-import { getItems } from '../../container/itemActions';
-import { getSuppliers } from '../../container/supplierActions';
+import { useEffect } from "react";
+import SingleCategory from "./SingleCategory";
+import { useDispatch, useSelector } from "react-redux";
+import { getCategories } from "../../container/categoryActions";
+import { getItems } from "../../container/itemActions";
+import { getSuppliers } from "../../container/supplierActions";
 
 const Categories = () => {
-    // Call dispatch
-    const dispatch = useDispatch();
+  // Call dispatch
+  const dispatch = useDispatch();
 
-    // useEffect is equivalent to componentDidMount()
-    useEffect(() => { 
-        dispatch(getCategories()); 
-        dispatch(getItems());
-        dispatch(getSuppliers());
-    }, []);
-    
-    // Get reducer from index.js
-    const category = useSelector((state) => state.category);
-    const item = useSelector((state) => state.item);
-    const supplier = useSelector((state) => state.supplier);
+  // useEffect is equivalent to componentDidMount()
+  useEffect(() => {
+    dispatch(getCategories());
+    // dispatch(getItems());
+    // dispatch(getSuppliers());
+  }, []);
 
-    // Deconstruct
-    // Follow the name of array in the state in deconstructuring.
-    const { categories } = category;
-    const { items } = item;
-    const { suppliers } = supplier;
+  // Get reducer from index.js
+  const category = useSelector((state) => state.category);
+  const item = useSelector((state) => state.item);
+  const supplier = useSelector((state) => state.supplier);
 
-    return (
-        categories.map((ctgry) => (                
-            <SingleCategory key={ctgry._id} category={ctgry} categoryID={ctgry.categoryID} 
-            items={items.filter(item => item.categoryID == ctgry.categoryID)}
-            suppliers={suppliers}/>
-        ))
-    )
-}
+  // Deconstruct
+  // Follow the name of array in the state in deconstructuring.
+  const { categories } = category;
+  const { items } = item;
+  const { suppliers } = supplier;
+
+  return categories.map((ctgry) => (
+    <SingleCategory
+      key={ctgry._id}
+      category={ctgry}
+      categoryID={ctgry.categoryID}
+      items={items.filter((item) => item.categoryID == ctgry.categoryID)}
+      suppliers={suppliers}
+    />
+  ));
+};
 
 export default Categories;
